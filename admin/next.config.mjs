fa4656a2ce115c66852dd8ca314772e55d4db4aa
warlord10328-design <warlord10328@gmail.com>
2025-12-22 17:38:03 +0530

    Update next.config.mjs

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
